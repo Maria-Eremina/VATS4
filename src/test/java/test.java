9229a8f43d1787a6a6bac3e62220f52e205177cd
Alexander <psasha050@yandex.ru>
2021-09-17 16:09:47 +0300

    Изменил ром и добавил опен гугл

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,4 +1,20 @@
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.CollectionCondition.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
 public class test {
 
     String ss = "qwe";
+
+    @Test
+    public void qwe1()
+    {
+        open("https://www.google.com/");
+
+    }
+
 }
